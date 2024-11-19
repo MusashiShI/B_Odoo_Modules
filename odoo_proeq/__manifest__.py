9be42b8eddeebfa879bf1bb3446098d094645ad9
jbeselga <beselgamain@gmail.com>
2024-11-19 11:57:20 +0000

    [IMP] Tutorial

--- odoo_proeq/__manifest__.py
+++ odoo_proeq/__manifest__.py
@@ -5,29 +5,4 @@
     'license': "LGPL-3",
     'version': '0.1',
     'category': 'Services',
-
-    'summary': """ Can add servers of proeq and show the status """,
-
-    'description': """
-        See the status of proeq servers
-    """,
-
-
-    'website': "http://www.beselgamodules.com",
-
-
-
-
-    'depends': ['base'],
-
-
-    'data': [
-
-        'views/views.xml',
-        'views/templates.xml',
-    ],
-
-    'demo': [
-        'demo/demo.xml',
-    ],
 }
