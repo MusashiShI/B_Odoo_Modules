9a3486e14ddf987531ba0078af73cbbb8fa73e3e
jbeselga <beselgamain@gmail.com>
2024-11-20 09:59:33 +0000

    [IMP] NEW FIELDS

--- odoo_proeq/models/server.py
+++ odoo_proeq/models/server.py
@@ -7,13 +7,15 @@ class ProeqServer(models.Model):
     _name = 'proeq.server'
     _description = 'Server'
 
-    # name, state, description, ip,
+    # name, state, description, ip, type ( ODOO, VUE, DATABASE )
 
     name = fields.Char(string="Name",  required=True)
-    state = fields.boolean(string="State", required = True,  readonly = True)
+    state = fields.Boolean(string="State", required = True,  readonly = True)
+    # Para analisar o state usa ping, caso esteja down usa wget para ter mais informações
     description =  fields.Char(string="Description",  required=True)
     ip = fields.Char(string="Ip",  required=True)
-    # Para analisar o state usa ping, caso esteja down usa wget para ter mais informações
+    type = fields.Selection([('odoo','Odoo'), ('vue','Vue'),('database','DataBase')], string="type")
+
 
 
 
