29b7a06b07853a7604cff4247aa85656d2845db0
jbeselga <beselgamain@gmail.com>
2024-11-20 10:11:05 +0000

    [IMP] MENU

--- odoo_proeq/__manifest__.py
+++ odoo_proeq/__manifest__.py
@@ -1,8 +1,13 @@
 # -*- coding: utf-8 -*-
 {
-    'name': "Odoo Proeq Monitoring",
-    'author': "Jose Beselga - ERPGAP SYSADM JUNIOR",
-    'license': "LGPL-3",
-    'version': '0.1',
-    'category': 'Services',
+    "name": "Odoo Proeq Monitoring",
+    "author": "Jose Beselga - ERPGAP SYSADM JUNIOR",
+    "license": "LGPL-3",
+    "version": '0.1',
+    "category": "Services",
+
+    "data": [
+
+        "views/menu.xml"
+    ]
 }
