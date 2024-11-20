d375e6249e306dff2599113eaa061c02c8bd9d51
jbeselga <beselgamain@gmail.com>
2024-11-20 09:33:46 +0000

    [IMP] NEW FIELDS

--- odoo_proeq/models/server.py
+++ odoo_proeq/models/server.py
@@ -5,4 +5,15 @@ from odoo import api, fields, models
 
 class ProeqServer(models.Model):
     _name = 'proeq.server'
-    _description = 'Server All'
+    _description = 'Server'
+
+    # name, state, description, ip,
+
+    name = fields.Char(string="Name",  required=True)
+    state = fields.boolean(string="State", required = True,  readonly = True)
+    description =  fields.Char(string="Description",  required=True)
+    ip = fields.Char(string="Ip",  required=True)
+    # Para analisar o state usa ping, caso esteja down usa wget para ter mais informações
+
+
+
