cac20b3c04bd8306c964f0e245b23907bc9652de
jbeselga <beselgamain@gmail.com>
2024-11-25 10:06:41 +0000

    [ADD] NEW CLASS FOR SAAS

--- odoo_proeq/models/server.py
+++ odoo_proeq/models/server.py
@@ -22,3 +22,18 @@ class ProeqServer(models.Model):
 
 
 
+class ProeqServer_Saas(models.Model):
+    _name = 'proeq.saas.server'
+    _inherit = ['mail.thread', 'mail.activity.mixin']
+    _description = 'Saas Server'
+
+    # name, state, description, ip, type ( ODOO, VUE, DATABASE )
+
+    name = fields.Char(string="Name",  required=True)
+    state = fields.Selection([('on','On'), ('off','Off'),('problems','Problems')], string="State")
+    # Para analisar o state usa ping, caso esteja down usa wget para ter mais informações
+    description =  fields.Char(string="Description",  required=True)
+    ip = fields.Char(string="Ip",  required=True)
+    type = fields.Selection([('odoo','Odoo'), ('vue','Vue'),('database','DataBase'),('locust','Locust')], string="Type")
+    odoo_version = fields.Selection([('18.0', '18.0'), ('17.0', '17.0'), ('16.0', '16.0')],string="Odoo Version")
+    ubuntu_version = fields.Selection([('24.04', '24.04'), ('22.04', '22.04')], string="Ubuntu Version")
